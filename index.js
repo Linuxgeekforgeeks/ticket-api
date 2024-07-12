3c00a427ba465fa5cf9333f0244ce95e5db31a27
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2024-07-11 22:53:21 -0700

    Testing Routes

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-const express=require("express");
-const mongoose=require("mongoose")
-const app=express()
-const port=5000
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const connectToMongo=()=>{
-    try {
-        mongoose.connect(process.env.MONGODB_URL)
-    } catch (error) {
-        console.log(error)
-    }
-}
+// Variables
+const app = express();
+dotenv.config();
+const port = 5000;
+
+app.get("/api/hello", (req, res) => {
+  const name = req.params;
+  res.send("Hello People on Youtube." + name);
+});
+
+app.get("/api/users", (req, res) => {
+  res.json({
+    "users": [
+      { username: "codeWithMudi", About: "This is me codeWithMudi." },
+      { username: "John Doe", About: "This is me John Doe" },
+    ],
+  });
+});
+
+const connectToMongo = () => {
+  try {
+    mongoose.connect(process.env.MONGODB_URL);
+    console.log("Connected to MongoDb Successfully.")
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 connectToMongo();
 
-
-app.listen(port,()=>{
-    console.log("Our Server is running on the port "+port)
-})
+app.listen(port, () => {
+  console.log("Our Server is running on the port " + port);
+});
