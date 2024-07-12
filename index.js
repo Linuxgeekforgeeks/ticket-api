e568b31e29843559bee561d76a6400d4f0f2907d
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2024-07-11 22:27:49 -0700

    Connect to Mongodb and Install dotenv

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express=require("express");
+const mongoose=require("mongoose")
 const app=express()
 const port=5000
+
+const connectToMongo=()=>{
+    try {
+        mongoose.connect(process.env.MONGODB_URL)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+connectToMongo();
+
 
 app.listen(port,()=>{
     console.log("Our Server is running on the port "+port)
