a200eea67a56952912f3921bb49613302ef13b8e
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2024-07-12 19:51:23 -0700

    start using bcrypt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,25 @@
 const express=require("express")
+const User=require("../models/userModel")
 
 const router=express.Router()
 
 router.post("/register",async(req,res)=>{
     const {username,password}=req.body
+    const user= await User.findOne({username});
+    if(user){
+       return res.status(400).json("user already Taken")
+    }
+
+
+    
+    const userData= await User({username,password})
+    const results=await userData.save()
+    res.json(results);
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
     res.json({userinfo:[username,password]})
 })
 router.post("/login",(req,res)=>{
