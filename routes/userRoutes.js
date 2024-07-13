6f501efbb4abc5d384cea7e644d6ca450ad5cd8d
Linuxgeekforgeeks <mahmoudalisaaa@gmail.com>
2024-07-13 11:21:52 -0700

    Started Login Functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,16 +18,29 @@ router.post("/register",async(req,res)=>{
 
     const userData= await User({username,password:hashPassword})
     const results=await userData.save()
-    res.json(results);
+    res.json(results)
+    res.json({message:"User Registered Sucessfully"});
     try {
         
     } catch (error) {
         console.log(error)
     }
-    res.json({userinfo:[username,password]})
 })
-router.post("/login",(req,res)=>{
-res.send("Your are Login Successfully.")
+router.post("/login",async(req,res)=>{
+    const {username,password}=req.body;
+    const existingUser= await User.findOne({username})
+    if(!existingUser){
+        res.status(400).json("User Does not Exists.")
+    }
+
+
+    const hashedPassword=existingUser.password
+    const isCorrectPass=await bcrypt.compare(password,hashedPassword)
+    if(!isCorrectPass){
+        res.json({message:"Wrong Credentials"})
+    }
+
+
 })
 
 
